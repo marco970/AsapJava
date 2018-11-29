0a5f73501999c3ff6b40a799e6263f88058318f9
Marcs <m.kuciak@gmail.com>
2018-11-29 17:26:25 +0100

    z domu

--- DoAsap/src/aSap/AbstractFormI.java
+++ DoAsap/src/aSap/AbstractFormI.java
@@ -13,20 +13,27 @@ import javax.swing.border.EmptyBorder;
 
 import net.miginfocom.swing.MigLayout;
 
-public abstract class AbstractFormI implements ActionListener {
+public class AbstractFormI implements ActionListener {
 	
 	private JFrame newFrame;
-	private JPanel panel;
+	protected JPanel panel;
+	
+	private JButton btnSave = new JButton("Zapisz");
+	private JButton btnCancel = new JButton("Anuluj");
+	private JButton btnNext = new JButton("Dalej");
+	private JButton btnBack = new JButton("Powrót");
 	
 
 	//private JButton btnBack = new JButton("Dalej");
-	protected JPanel contentPane;
+	public JPanel contentPane;
 
 	public AbstractFormI(String title, String messageLabel)	{
+		
 		newFrame = new JFrame(title);
 		newFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		newFrame.setVisible(true);
 		newFrame.setBounds(100, 100, 450, 600);
+		//panel
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		newFrame.setContentPane(contentPane);
@@ -34,29 +41,22 @@ public abstract class AbstractFormI implements ActionListener {
 		panel = new JPanel();
 		contentPane.add(panel, "cell 0 0, grow");
 		panel.setLayout(new MigLayout("", "[][][]", "[][][]"));
-		
-
-		//btnBack.addActionListener(this);
-		
-		
+		//tytuł ramki
 		JLabel titleLab = new JLabel(messageLabel);
-		//titleLab.setFont(new Font("Tahoma", Font.BOLD, 16));
-		contentPane.add(titleLab, "dock north");
-		//contentPane.add(btnBack, "cell 0 1");
-		
+		titleLab.setFont(new Font("Tahoma", Font.BOLD, 16));
 		//przyciski
-		//contentPane.add(btnCancel, "cell 0 1");
+		btnSave.addActionListener(this);
+		btnCancel.addActionListener(this);
+		//btnNext.setText("Dalej");
+		btnNext.addActionListener(this);
+		btnBack.addActionListener(this);
 		
-		//contentPane.add(btnNext, "cell 0 1");
-		//contentPane.add(btnSave, "cell 0 1");
-		
-		
-
-		//btnBack.setVisible(false);
-
-		
-		
-		
+		contentPane.add(btnCancel, "cell 0 1");
+		contentPane.add(btnBack, "cell 0 1");
+		contentPane.add(btnNext, "cell 0 1");
+		contentPane.add(btnSave, "cell 0 1");
+		btnSave.setEnabled(false);
+		btnBack.setVisible(false);
 		
 	}//---koniec konstruktora
 	
